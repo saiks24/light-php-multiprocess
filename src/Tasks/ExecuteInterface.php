8784be75c120f3e8589b20016e69c9ffa2e628c8
saiks24 <saiks1337@yandex.ru>
2019-04-18 12:22:22 +0300

    Init ExecuteInterface

--- src/Tasks/ExecuteInterface.php
+++ src/Tasks/ExecuteInterface.php
@@ -1,9 +1,30 @@
 <?php
+namespace Saiks24\Tasks;
 
 /**
  * Interface ExecuteInterface
  */
 interface ExecuteInterface
 {
+
+    /** Task init method
+     * @return bool
+     */
+    public function init() : bool;
+
+    /** Start task execute
+     * @return mixed
+     */
+    public function process();
+
+    /** On success end of execute
+     * @return mixed
+     */
+    public function onSuccess();
+
+    /** On fail end of execute
+     * @return mixed
+     */
+    public function onFailure();
 
 }
