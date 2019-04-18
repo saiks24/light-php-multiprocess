8b1daf508540e1537360ccb0ab1af078d939020a
saiks24 <saiks1337@yandex.ru>
2019-04-18 12:31:03 +0300

    ProcessPull::add realise

--- src/Pool/ProcessPull.php
+++ src/Pool/ProcessPull.php
@@ -8,11 +8,17 @@ class ProcessPull
     /** @var SplObjectStorage */
     private $threadStorage;
 
+    /** Attach new process to pull
+     * @param ThreadInterface $thread
+     */
     public function add(ThreadInterface $thread)
     {
-
+        $this->threadStorage->attach($thread);
     }
 
+    /**
+     * Start pull process execute
+     */
     public function start()
     {
         /** @var ThreadInterface $thread */
